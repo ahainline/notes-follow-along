c3558df5c6a5c48e5adb02e62c96185848fa2de3
Ashley Hainline <aetindle@gmail.com>
2025-09-05 14:39:38 -0500

    add Note model, repository, and service

---
+++ src/main/java/com/secure/notes_follow_along/repositories/NoteRepository.java
@@ -0,0 +1,9 @@
+package com.secure.notes_follow_along.repositories;
+
+import com.secure.notes_follow_along.models.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findByOwnerUsername(String ownerUsername);
+}

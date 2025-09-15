984e9f0a7f9d9a84665d524d1158ac409f43f57a
Ashley Hainline <aetindle@gmail.com>
2025-09-15 12:13:59 -0500

    add custom User model and repository, add custom Role model

---
+++ src/main/java/com/secure/notes_follow_along/models/AppRole.java
@@ -0,0 +1,6 @@
+package com.secure.notes_follow_along.models;
+
+public enum AppRole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/secure/notes_follow_along/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.secure.notes_follow_along.repositories;
+
+
+import com.secure.notes_follow_along.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String username);
+}
